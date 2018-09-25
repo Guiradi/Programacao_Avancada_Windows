21180b6b62bc03d96bbf6b3d65c85a1f5d4de24b
Guilherme Ponsoni Ferreira <guiradi@hotmail.com>
2018-09-25 20:06:50 -0300

    Aula sobre semaforo e mutex

--- 05-Semaforo-Mutex/1-Semaforo/TesteSemaforo.dpr
+++ 05-Semaforo-Mutex/1-Semaforo/TesteSemaforo.dpr
@@ -4,14 +4,57 @@ uses
   Forms,
   Unit1 in 'Unit1.pas' {Form1TesteSemaforo1234},
   Unit2 in 'Unit2.pas' {Form2},
-  Unit3 in 'Unit3.pas' {Form3};
+  Unit3 in 'Unit3.pas' {Form3},
+  Windows {Para usar HWND, FindWindow, GetWindowLong, IsIconic, ShowWindow, ..., Create Semaphore, ...};
 
 {$R *.res}
 
+var
+  Semaforo: THandle;
+
+procedure RestoreWindow(aFormName : string);
+var
+  Janela, Aplicacao: HWND;
 begin
+  // Procura janela cuja fôrma tem o tipo TForm1TesteSemaforo1234 (usado na fôrma da Unit1)
+  Janela := FindWindow(PChar(aFormName), nil);
+  // Se já existe programa com esta janela sendo executada
+  if (Janela <> 0) then
+  begin
+    // Retém a aplicação que tem a janela em questão
+    Aplicacao := GetWindowLong(Janela, GWL_HWNDPARENT);
+    // Se estiver minimizada na barra de botões
+    if IsIconic(Aplicacao) then
+      // Mostra a janela
+      ShowWindow(Aplicacao, SW_RESTORE);
+      // Coloca a janela na frente dos outros programas
+      SetForegroundwindow(Aplicacao);
+      // Emite beep (não é necessário)
+      //Valores padrão do beep: 0 Default, 16 Critical, 32 Question, 48 Exclamation, 64 Asterisc
+      MessageBeep(64);
+    end;
+end;
+
+begin
+
+  // Não iniciar duas vezes
+  // É claro que o programa inicia, mas deve ser interrompido se outra instância estiver executando
+  // Esta interrupção deve ser antes de apresentar qualquer janela da segunda instância
+  Semaforo := CreateSemaphore(nil, 0, 1, 'IdentificadorQueNinguemMaisVaiUsarParaTesteSemaforo');
+  if((Semaforo <> 0) and (GetLastError = ERROR_ALREADY_EXISTS)) then
+  begin
+    // Se já está executando, traz a janela principal da instância que está executando para frente
+    RestoreWindow('TForm1TesteSemaforo1234');
+    CloseHandle(Semaforo);
+    Halt;
+  end;
+  
   Application.Initialize;
+  Application.Title := 'Teste Semaforo - só uma instância';
   Application.CreateForm(TForm1TesteSemaforo1234, Form1TesteSemaforo1234);
   Application.CreateForm(TForm2, Form2);
   Application.CreateForm(TForm3, Form3);
   Application.Run;
+
+  CloseHandle(Semaforo);
 end.
